2353ad2aed6c5c7a797d6162d49f6cd4b4092f37
MaxiCattaneoCvetic <cveticmaxi97@gmail.com>
2024-04-27 09:25:48 -0300

    add repository

---
+++ internal/user/repository.go
@@ -0,0 +1,47 @@
+package user
+
+import (
+	"context"
+	"goWeb/v2/internal/domain"
+	"log"
+)
+
+// generamos una estructura para la base de datos
+type DB struct {
+	Users     []domain.User // sera un slice de user
+	MaxUserID uint64
+}
+
+// generamos la interfaz para la base de datos
+type (
+	Repository interface {
+		Create(ctx context.Context, user *domain.User) error // Metodo create, que tendra el context  y despues vamos a recibir por parametro un usuario. (un puntero de usuario) y puede devolver un error
+		GetAll(ctx context.Context) ([]domain.User, error)
+	}
+	// estos campos que tendremos en repositorio lo manejamos en minuscula porque son campos privados
+	repo struct {
+		db  DB
+		log *log.Logger // para manejar el log
+	}
+)
+
+func newRepo(db DB, log *log.Logger) Repository {
+	return &repo{
+		db:  db,
+		log: log,
+	}
+}
+
+func (r *repo) Create(ctx context.Context, user *domain.User) error {
+
+	r.db.MaxUserID++                       // generamos un ID
+	user.ID = r.db.MaxUserID               // setemos el ID generado al usuario  nuevo
+	r.db.Users = append(r.db.Users, *user) // lo agregamos al slice de usuarios
+	r.log.Println("Repository log: User created", user)
+	return nil
+
+}
+func (r *repo) GetAll(ctx context.Context) ([]domain.User, error) {
+	log.Println("Repository log: Get all users")
+	return r.db.Users, nil
+}
